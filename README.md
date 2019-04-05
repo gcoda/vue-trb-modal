f4b8024fe990f4de111a4c8fb194c2ee985b7aa6
gcoda <gcoding@gmail.com>
2019-04-06 00:45:16 +0300

    readme

--- README.md
+++ README.md
@@ -17,10 +17,12 @@
       </template>
       <h2>on hover</h2>
     </modal>
-    <modal tag="li" class="orange" :toggle-on-click="false">
+    <modal tag="ul" class="orange" :toggle-on-click="false">
       <template #activator="{on, active, hide}">
-        <h1 v-on="on">inside li.orange</h1>
-        <h3 v-if="active" @click="hide">close button</h3>
+        <li v-on="on">inside ul.orange &gt; li</li>
+        <ul v-if="active">
+          <li @click="hide">close button</li>
+        </ul>
       </template>
       <h2>toggleOnClick="false"</h2>
     </modal>
--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
   <div id="app">
     <modal>
       <template #activator="{on}">
-        <h1 v-on="on">activator</h1>
+        <h1 v-on="on">click</h1>
       </template>
       <h2>on click</h2>
     </modal>
     <modal hover>
       <template #activator="{on}">
-        <h1 v-on="on">activator</h1>
+        <h1 v-on="on">mouse enter</h1>
       </template>
       <h2>on hover</h2>
     </modal>
-    <modal class="orange" :toggle-on-click="false">
+    <modal tag="ul" class="orange" :toggle-on-click="false">
       <template #activator="{on, active, hide}">
-        <h1 v-on="on">activator</h1>
-        <h3 v-if="active" @click="hide">close button</h3>
+        <li v-on="on">inside ul.orange &gt; li</li>
+        <ul v-if="active">
+          <li @click="hide">close button</li>
+        </ul>
       </template>
       <h2>toggleOnClick="false"</h2>
     </modal>
@@ -34,12 +36,16 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 20rem;
+  margin-left: auto;
+  margin-right: auto;
   margin-top: 60px;
 }
 </style>
